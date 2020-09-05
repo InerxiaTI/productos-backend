6e577eebc4d9b8e301f3bf39899a7c454c059be7
Juan Guillermo Hernandez Alarcon <juanguillermoalarcon@gmail.com>
2020-09-04 22:35:48 -0500

    Primer commit

---
+++ src/test/java/co/com/inerxia/productos/ProductosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.inerxia.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
