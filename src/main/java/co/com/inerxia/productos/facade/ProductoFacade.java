83c07182c950ecc20d25076cce267b8bbc7f25b1
kellytbn <kellytbn14@gmail.com>
2020-09-05 14:22:07 -0500

    Se agregan los facade

---
+++ src/main/java/co/com/inerxia/productos/facade/ProductoFacade.java
@@ -0,0 +1,44 @@
+package co.com.inerxia.productos.facade;
+
+import co.com.inerxia.productos.facade.dto.ProductoDto;
+import co.com.inerxia.productos.facade.mapper.ProductoMapper;
+import co.com.inerxia.productos.service.ProductoService;
+
+import java.util.List;
+
+public class ProductoFacade {
+
+    private ProductoMapper productoMapper;
+    private ProductoService productoService;
+
+    private ProveedorFacade proveedorFacade;
+
+    public ProductoFacade(ProductoMapper productoMapper, ProductoService productoService, ProveedorFacade proveedorFacade) {
+        this.productoMapper = productoMapper;
+        this.productoService = productoService;
+        this.proveedorFacade = proveedorFacade;
+    }
+
+    public ProductoDto findById(Integer id){
+        return productoMapper.toDto(productoService.findById(id));
+    }
+
+    public List<ProductoDto> findAll(){
+        return productoMapper.toDto(productoService.findAll());
+    }
+
+    public ProductoDto create(ProductoDto productoDto){
+        proveedorFacade.findById(productoDto.getIdProveedor_fk());
+        return productoMapper.toDto(productoService.create(productoMapper.toEntity(productoDto)));
+    }
+
+    public ProductoDto edit(ProductoDto productoDto){
+        proveedorFacade.findById(productoDto.getIdProveedor_fk());
+        return productoMapper.toDto(productoService.edit(productoMapper.toEntity(productoDto)));
+    }
+
+    public void delete(Integer id) {
+        productoService.delete(id);
+    }
+
+}
